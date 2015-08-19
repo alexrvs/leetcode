3cc2cf1dfd8a9a1d690ab6869731edabb6131251
hawkphantomnet <hawkphantomnet@gmail.com>
2015-08-19 23:13:39 +0800

    add cpp solution for Palindrome Number

---
+++ PalindromeNumber/Solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int left = 1, right = 10, tx = x/10;
+        while (tx > 0) {
+            tx /= 10;
+            left *= 10;
+        }
+        while (left >= right) {
+            if(x / left != x % right / (right / 10)) {
+              return false;
+            }
+            x %= left;
+            x = x - x%right;
+            left /= 10;
+            right *= 10;  
+        }
+        return true;
+    }
+};
