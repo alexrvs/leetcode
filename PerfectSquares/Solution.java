4f5b2124f9d48a44b786e26be8b3829c0e9de587
hawkphantomnet <hawkphantomnet@gmail.com>
2015-09-10 16:50:44 +0800

    add javascript solution for Perfect Squares

---
+++ PerfectSquares/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int numSquares(int n) {
+        int[] nums = new int[n+1];
+        int least = 0;
+        int num = 0;
+        for (int i = 1; i <= n; i++) {
+            least = i;
+            for (int x = 1; x * x <= i; x++) {
+                num = 1 + nums[ i - x * x ];
+                if (least > num) {
+                    least = num;
+                }
+            }
+            nums[i] = least;
+        }
+        return nums[n];
+    }
+}
