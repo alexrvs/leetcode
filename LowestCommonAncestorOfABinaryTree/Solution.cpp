9bd7cb768a2ab5ffc58e3af19ad4ec61cc42e0b0
hawkphantomnet <hawkphantomnet@gmail.com>
2015-09-14 10:56:58 +0800

    fix cpp solution for Lowest Common Ancestor Of A Binary Tree

--- LowestCommonAncestorOfABinaryTree/Solution.cpp
+++ LowestCommonAncestorOfABinaryTree/Solution.cpp
@@ -13,7 +13,7 @@ public:
         if (root == NULL || root == p || root == q) return root;
         TreeNode* left = lowestCommonAncestor(root->left, p, q);
         TreeNode* right = lowestCommonAncestor(root->right, p, q);
-        if (left != NULL and right != NULL) {
+        if (left != NULL && right != NULL) {
             return root;
         } else if (left != NULL) {
             return left;
