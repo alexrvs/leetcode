f15f4e6a5a31efdd63e80cd48995a2994d5a46c3
hawkphantomnet <hawkphantomnet@gmail.com>
2015-09-15 11:45:23 +0800

    add cpp solution for Jump Game

---
+++ JumpGame/Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int size = nums.size();
+        if (size == 0) return false;
+        int reachable = nums[0];
+        int can_reach = 0;
+        for (int i = 0; i < size && i <= reachable; i++) {
+            if (i == size - 1) return true;
+            can_reach = i + nums[i];
+            if (can_reach > reachable) reachable = can_reach;
+        }
+        return false;
+    }
+};
