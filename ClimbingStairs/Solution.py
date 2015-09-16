15614ecdf35c5d32396d5968f01ef448162d855b
hawkphantomnet <hawkphantomnet@gmail.com>
2015-09-16 19:56:53 +0800

    add python solution for Climbing Stairs

---
+++ ClimbingStairs/Solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 2:
+            return n
+        ways = [ 0 ] * (n + 1)
+        ways[1] = 1
+        ways[2] = 2
+        for i in range(3, n+1):
+            ways[i] = ways[i - 1] + ways[i - 2]
+        return ways[n]
