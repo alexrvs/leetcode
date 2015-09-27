958f015981a2865897debb1caa58e649ce900260
hawkphantomnet <hawkphantomnet@gmail.com>
2015-09-27 23:40:39 +0800

    add java solution for Binary Tree Inorder Traversal

---
+++ BinaryTreeInorderTraversal/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    private void helper(TreeNode node, List<Integer> vals) {
+        if (node == null) return;
+        helper(node.left, vals);
+        vals.add(node.val);
+        helper(node.right, vals);
+        return;
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> vals = new ArrayList<Integer>();
+        helper(root, vals);
+        return vals;
+    }
+}
