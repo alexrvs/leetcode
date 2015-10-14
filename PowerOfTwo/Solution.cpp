fa2c6c677bb78528e763c9d27255758cc4fdddc1
hawkphantomnet <hawkphantomnet@gmail.com>
2015-10-14 21:39:28 +0800

    add cpp solution for Power Of Two

---
+++ PowerOfTwo/Solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int cnt = 0;
+        while(n > 0) {
+            if (n & 0x0001) cnt++;
+            n >>= 1;
+        }
+        if (cnt == 1) return true;
+        return false;
+    }
+};
