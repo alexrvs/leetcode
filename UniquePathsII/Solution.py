824a1f5294cf84c85fbe7dc8f73fe2b2c64a174d
hawkphantomnet <hawkphantomnet@gmail.com>
2015-10-27 23:49:14 +0800

    add python solution for Unique Paths II

---
+++ UniquePathsII/Solution.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        m = len(obstacleGrid)
+        if m == 0:
+            return 0
+        n = len(obstacleGrid[0])
+        p = [ [ 0 ] * n ] * m
+        for i in range(0, m):
+            if obstacleGrid[i][0] != 1:
+                p[i][0] = 1
+            else:
+                break
+        for i in range(0, n):
+            if obstacleGrid[0][i] != 1:
+                p[0][i] = 1
+            else:
+                break
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] != 1:
+                    a = 0 if obstacleGrid[i - 1][j] else p[i - 1][j]
+                    b = 0 if obstacleGrid[i][j - 1] else p[i][j - 1]
+                    p[i][j] = a + b
+        return p[m - 1][n - 1]
