05c40313040e81bdf47ef1dd0bfcee7d7207ef3d
hawkphantomnet <hawkphantomnet@gmail.com>
2015-11-10 22:31:00 +0800

    add python solution for Range Sum Query - Immutable

---
+++ RangeSumQueryImmutable/Solution.py
@@ -0,0 +1,27 @@
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        self.nums = nums
+        self.sums = []
+        self.sums.append(0)
+        l = len(nums)
+        for i in range(1, l):
+            self.sums.append(self.sums[i - 1] + nums[i - 1])
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return self.sums[j] - self.sums[i] + self.nums[j]
+
+
+# Your NumArray object will be instantiated and called as such:
+# numArray = NumArray(nums)
+# numArray.sumRange(0, 1)
+# numArray.sumRange(1, 2)
