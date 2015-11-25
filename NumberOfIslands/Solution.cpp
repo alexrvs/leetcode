26b2b31bacb80489d60bfb3fba84cd31bee9ce90
hawkphantomnet <hawkphantomnet@gmail.com>
2015-11-25 23:20:10 +0800

    add cpp solution for Number Of Islands

---
+++ NumberOfIslands/Solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    void markVisited(vector<vector<char> >& grid, int i, int j) {
+        grid[i][j] = '0';
+        int r = grid.size();
+        if (r == 0) return;
+        int c = grid[0].size();
+        for (int a = -1; a <= 1; a += 2) {
+            int ia = i + a;
+            if (0 <= ia && ia < r && grid[ia][j] == '1') markVisited(grid, ia, j);
+        }
+        for (int b = -1; b <= 1; b += 2) {
+            int jb = j + b;
+            if (0 <= jb && jb < c && grid[i][jb] == '1') markVisited(grid, i, jb);
+        }
+    }
+public:
+    int numIslands(vector<vector<char> >& grid) {
+        int num = 0;
+        int r = grid.size();
+        if (r == 0) return 0;
+        int c = grid[0].size();
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == '1') {
+                    num++;
+                    markVisited(grid, i, j);
+                }
+            }
+        }
+        return num;
+    }
+};
