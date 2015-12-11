6f8bc34fae3f51576d2f4bb3677999610787f9dc
hawkphantomnet <hawkphantomnet@gmail.com>
2015-12-11 23:35:55 +0800

    modify cpp solution for 3Sum

--- 3Sum/Solution.cpp
+++ 3Sum/Solution.cpp
@@ -3,9 +3,7 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int> > res;
         int size = nums.size();
-        if (size < 3) return res;
         sort(nums.begin(), nums.end());
-        int prev = 0;
         for (int i = 0; i < size - 2; ) {
             for (int j = i + 1, k = size - 1; j < k; ) {
                 if (nums[i] + nums[j] + nums[k] == 0) {
@@ -14,18 +12,14 @@ public:
                     r.push_back(nums[j]);
                     r.push_back(nums[k]);
                     res.push_back(r);
-                    prev = nums[j];
-                    while (nums[j] == prev && j < k) j++;
+                    while (j < k && nums[++j] == nums[j - 1]);
                 } else if (nums[i] + nums[j] + nums[k] < 0) {
-                    prev = nums[j];
-                    while (nums[j] == prev && j < k) j++;
+                    while (j < k && nums[++j] == nums[j - 1]);
                 } else {
-                    prev = nums[k];
-                    while (nums[k] == prev && j < k) k--;
+                    while (j < k && nums[--k] == nums[k + 1]);
                 }
             }
-            prev = nums[i];
-            while (nums[i] == prev && i < size) i++;
+            while (i < size && nums[++i] == nums[i - 1]);
         }
         return res;
     }
