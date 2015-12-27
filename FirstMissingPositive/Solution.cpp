0a0e695c8804e77d19e72f34ce08688b32c6076b
hawkphantomnet <hawkphantomnet@gmail.com>
2015-12-27 17:48:38 +0800

    add cpp solution for First Missing Positive

---
+++ FirstMissingPositive/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int size = nums.size();
+        int cur = 0;
+        int tmp = 0;
+        for (int i = 0; i < size; i++) {
+            cur = nums[i];
+            while (cur >= 1 && cur <= size && nums[cur - 1] != cur) {
+                tmp = nums[cur - 1];
+                nums[cur - 1] = cur;
+                cur = tmp;
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return size + 1;
+    }
+};
