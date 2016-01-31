ff5b5b17ec96db6b7828ed5ffbdd7929698c8533
hawkphantomnet <hawkphantomnet@gmail.com>
2016-01-31 23:17:21 +0800

    add java solution for Minimum Path Sum

---
+++ MinimumPathSum/Solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        int h = grid.length;
+        if (h == 0) return 0;
+        int w = grid[0].length;
+        int up = 0;
+        int left = 0;
+        for (int i = 1; i < h; i++) {
+            grid[i][0] += grid[i - 1][0];
+        }
+        for (int j = 1; j < w; j++) {
+            grid[0][j] += grid[0][j - 1];
+        }
+        for (int i = 1; i < h; i++) {
+            for (int j = 1; j < w; j++) {
+                up = grid[i - 1][j];
+                left = grid[i][j - 1];
+                grid[i][j] += (up < left ? up : left);
+            }
+        }
+        return grid[h - 1][w - 1];
+    }
+}
