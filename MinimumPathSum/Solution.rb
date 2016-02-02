b55b99fa363d9f09999a59b444b9bf8e22583f0d
hawkphantomnet <hawkphantomnet@gmail.com>
2016-02-02 10:01:25 +0800

    fix indent issue

--- MinimumPathSum/Solution.rb
+++ MinimumPathSum/Solution.rb
@@ -12,12 +12,12 @@ def min_path_sum(grid)
     end
     1.upto(h - 1) do | i |
         1.upto(w - 1) do | j |
-        if grid[i - 1][j] < grid[i][j - 1] then
-            grid[i][j] += grid[i - 1][j]
-        else
-            grid[i][j] += grid[i][j - 1]
+            if grid[i - 1][j] < grid[i][j - 1] then
+                grid[i][j] += grid[i - 1][j]
+            else
+                grid[i][j] += grid[i][j - 1]
+            end
         end
-    end
     end
     return grid[h - 1][w - 1]
 end
