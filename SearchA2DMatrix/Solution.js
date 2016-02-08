e6d51f98a5b49c3e2c31fbd4fe1e3709ea3fea04
hawkphantomnet <hawkphantomnet@gmail.com>
2016-02-08 15:37:03 +0800

    add javascript solution for Search A 2D Matrix

---
+++ SearchA2DMatrix/Solution.js
@@ -0,0 +1,37 @@
+var searchRow = function(row, target) {
+    var l = 0;
+    var r = row.length;
+    var m = 0;
+    while (l < r) {
+        m = l + Math.floor((r - l) / 2 );
+        if (row[m] < target) {
+            l = m + 1;
+        } else if (row[m] == target) {
+            return true;
+        } else {
+            r = m;
+        }
+    }
+    return false;
+}
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    var l = 0;
+    var r = matrix.length;
+    var m = 0;
+    while (l < r) {
+        m = l + Math.floor((r - l) / 2);
+        if (matrix[m][0] <= target) {
+            l = m + 1;
+        } else {
+            r = m;
+        }
+    }
+    if (l === 0) return false;
+    return searchRow(matrix[l - 1], target);
+};
