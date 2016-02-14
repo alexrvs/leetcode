e971f4a2f62a3f58390bbab3347ac91465300f84
hawkphantomnet <hawkphantomnet@gmail.com>
2016-02-14 23:40:12 +0800

    add java solution for Unique Binary Search Trees

---
+++ UniqueBinarySearchTrees/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int numTrees(int n) {
+        int[] nums = new int[n + 1];
+        nums[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                nums[i] += nums[j - 1] * nums[i - j];
+            }
+        }
+        return nums[n];
+    }
+}
