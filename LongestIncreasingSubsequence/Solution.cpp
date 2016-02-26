f96f3684c36bfd4027e61ea9ed86a7163c456b2c
hawkphantomnet <hawkphantomnet@gmail.com>
2016-02-27 01:10:37 +0800

    add cpp solution for Longest Increasing Subsequence

--- LongestIncreasingSubsequence/Solution.cpp
+++ LongestIncreasingSubsequence/Solution.cpp
@@ -1,23 +1,31 @@
 class Solution {
-    public:
-        int lengthOfLIS(vector<int>& nums) {
-            int n = nums.size();
-            if (n < 1) return 0;
-            vector<int> cnts(n, 1);
-            int max = 1;
-            for (int i = 1; i < n; i++) {
-                int myMax = 0;
-                for(int j = i - 1; j >= 0; j--) {
-                    if (nums[j] < nums[i]) {
-                        cnts[i] = cnts[j] + 1 > myMax ? cnts[j] + 1 : myMax;
-                        if (max < cnts[i]) max = cnts[i];
-                    }
-                }
+private:
+    int search(vector<int>& seq, int l, int r, int target) {
+        int m = 0;
+        while (l < r) {
+            m = l + (r - l) / 2;
+            if (seq[i] < target) {
+                l = m + 1;
+            } else {
+                r = m;
             }
-            for (int i = 0; i < n; i++) {
-                cout << cnts[i] << " ";
-            }
-            cout << endl;
-            return max;
         }
+        return l - 1;
+    }
+
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 1) return 0;
+        vector<int> seq(n, 0);
+        int seqLen = 0;
+        for (int i = 0; i < n; i++) {
+            int pos = search(seq, 0, seqLen, nums[i]) + 1;
+            seq[pos] = nums[i];
+            if (pos == seqLen) {
+                seqLen = pos + 1;
+            }
+        }
+        return seqLen;
+    }
 };
