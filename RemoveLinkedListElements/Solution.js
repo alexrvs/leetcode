6f6f1163fcdfd3e3de6ebc4b67cdf83118d9b2d9
hawkphantomnet <hawkphantomnet@gmail.com>
2016-03-04 23:53:40 +0800

    add javascript solution for Remove Linked List

---
+++ RemoveLinkedListElements/Solution.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    var dummy = new ListNode();
+    dummy.next = head;
+    var cur = dummy;
+    while (cur.next !== null) {
+        if (cur.next.val === val) {
+            cur.next = cur.next.next;
+        } else {
+            cur = cur.next;
+        }
+    }
+    return dummy.next;
+};
