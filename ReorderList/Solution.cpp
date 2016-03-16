f6cf227b6285dd5239a36dd9327f6706ee0c6bfc
hawkphantomnet <hawkphantomnet@gmail.com>
2016-03-16 23:38:51 +0800

    add cpp solution for Reorder List

--- ReorderList/Solution.cpp
+++ ReorderList/Solution.cpp
@@ -7,39 +7,39 @@
  * };
  */
 class Solution {
-    private:
-        ListNode* reverseList(ListNode* head) {
-            ListNode * reverse = NULL;
-            ListNode * node = head;
-            while (node) {
-                head = node->next;
-                node->next = reverse;
-                reverse = node;
-                node = head;
-            }
-            return reverse;
+private:
+    ListNode* reverseList(ListNode* head) {
+        ListNode * reverse = NULL;
+        ListNode * node = head;
+        while (node) {
+            head = node->next;
+            node->next = reverse;
+            reverse = node;
+            node = head;
         }
+        return reverse;
+    }
 
-    public:
-        void reorderList(ListNode* head) {
-            if (head == NULL || head->next == NULL) return;
-            ListNode* slow = head;
-            ListNode* fast = head;
-            ListNode* tmp = NULL;
-            while (fast->next != NULL && fast->next->next != NULL) {
-                fast = fast->next->next;
-                slow = slow->next;
-            }
-            fast = slow->next;
-            slow->next = NULL;
-            fast = reverseList(fast);
-            while (fast != NULL) {
-                slow = head->next;
-                head->next = fast;
-                fast = fast->next;
-                head->next->next = slow;
-                head = slow;
-            }
-            return;
+public:
+    void reorderList(ListNode* head) {
+        if (head == NULL || head->next == NULL) return;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* tmp = NULL;
+        while (fast->next != NULL && fast->next->next != NULL) {
+            fast = fast->next->next;
+            slow = slow->next;
         }
+        fast = slow->next;
+        slow->next = NULL;
+        fast = reverseList(fast);
+        while (fast != NULL) {
+            slow = head->next;
+            head->next = fast;
+            fast = fast->next;
+            head->next->next = slow;
+            head = slow;
+        }
+        return;
+    }
 };
