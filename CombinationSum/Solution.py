73b977774e820822cca7c1e0c1ba0989f8bc9b63
hawkphantomnet <hawkphantomnet@gmail.com>
2016-03-26 00:07:18 +0800

    add python solution for Combination Sum

---
+++ CombinationSum/Solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def helper(self, candidates, path, index, sum, res):
+        if sum < 0:
+            return
+        if sum == 0:
+            res.append(list(path))
+        l = len(candidates)
+        for i in range(index, l):
+            path.append(candidates[i])
+            self.helper(candidates, path, i, sum - candidates[i], res)
+            path.pop()
+
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        path = []
+        self.helper(candidates, path, 0, 0, res)
+        return res
