6e1aac6e04118849bf62490261b6f298b7cdd534
hawkphantomnet <hawkphantomnet@gmail.com>
2016-04-14 23:50:16 +0800

    add c solution for Populating Next Right Pointers In Each Node

---
+++ PopulatingNextRightPointersInEachNode/Solution.c
@@ -0,0 +1,15 @@
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  struct TreeLinkNode *left, *right, *next;
+ * };
+ *
+ */
+void connect(struct TreeLinkNode *root) {
+    if (root == NULL) return;
+    if (root->left != NULL) root->left->next = root->right;
+    if (root->right != NULL && root->next != NULL) root->right->next = root->next->left;
+    connect(root->left);
+    connect(root->right);
+}
