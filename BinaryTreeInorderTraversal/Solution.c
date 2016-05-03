f7cd2a935b19d33dc51807818a72305044e71354
hawkphantomnet <hawkphantomnet@gmail.com>
2016-05-03 23:02:15 +0800

    add c solution for Binary Tree Inorder Traversal

---
+++ BinaryTreeInorderTraversal/Solution.c
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int count(struct TreeNode* node) {
+    if (node == NULL) return 0;
+    return 1 + count(node->left) + count(node->right);
+}
+
+void helper(struct TreeNode* node, int* vals, int* index) {
+    if (node == NULL) return;
+    helper(node->left, vals, index);
+    vals[(*index)] = node->val;
+    (*index)++;
+    helper(node->right, vals, index);
+    return;
+}
+
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    *returnSize = count(root);
+    int* vals = (int*) malloc((*returnSize) * sizeof(int));
+    int index = 0;
+    helper(root, vals, &index);
+    return vals;
+}
