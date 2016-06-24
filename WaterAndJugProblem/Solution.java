edb52e92368693f10d5bdb9feca1a762dc555ffa
hawkphantomnet <hawkphantomnet@gmail.com>
2016-06-25 01:36:19 +0800

    add java solution for Water And Jug Problem

---
+++ WaterAndJugProblem/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    private int myGCD(int x, int y) {
+        return (y == 0 ? x : myGCD(y, x % y));
+    }
+
+    public boolean canMeasureWater(int x, int y, int z) {
+        if (x == 0 && y == z) return true;
+        if (y == 0 && x == z) return true;
+        if (x == z || y == z) return true;
+        if (z > x + y) return false;
+        return z % myGCD(x, y) == 0;
+    }
+}
