d6c434248b9adb3c7cf991ab5e3a7fe66baa609d
hawkphantomnet <hawkphantomnet@gmail.com>
2016-06-26 20:41:44 +0800

    add java solution for Valid Perfect Square

---
+++ ValidPerfectSquare/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        if (num <= 1) return true;
+        int l = 1;
+        int r = num / 2;
+        int m = 0;
+        while (l < r) {
+            m = l + (r - l) / 2;
+            if (m < num / m) l = m + 1;
+            else r = m;
+        }
+        return (l == num / l && num % l == 0);
+    }
+}
