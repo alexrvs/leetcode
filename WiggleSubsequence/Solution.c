8051bd2db1b56d92dcc7a1f1678adc85ff81ddf9
hawkphantomnet <hawkphantomnet@gmail.com>
2016-07-21 22:55:27 +0800

    add c soluiton for Wiggle Subsequence

---
+++ WiggleSubsequence/Solution.c
@@ -0,0 +1,18 @@
+int wiggleMaxLength(int* nums, int numsSize) {
+    int size = numsSize;
+    if (size <= 1) return size;
+    int comm = nums[0];
+    int len = 1;
+    int prev = 0;
+    for (int i = 1; i < size; i++) {
+        if (prev >= 0 && nums[i] < comm) {
+            prev = -1;
+            len++;
+        } else if (prev <= 0 && nums[i] > comm) {
+            prev = 1;
+            len++;
+        }
+        comm = nums[i];
+    }
+    return len;
+}
