43ed823c8604a516d5194c36d8f17312e5a9fc81
hawkphantomnet <hawkphantomnet@gmail.com>
2016-08-01 23:29:30 +0800

    add c solution for Rectangle Area

---
+++ RectangleArea/Solution.c
@@ -0,0 +1,12 @@
+int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+    int area1 = (C - A) * (D - B);
+    int area2 = (G - E) * (H - F);
+    int inL = A > E ? A : E;
+    int inR = C < G ? C : G;
+    int inB = B > F ? B : F;
+    int inT = D < H ? D : H;
+    int inW = inR > inL ? inR - inL : 0;
+    int inH = inT > inB ? inT - inB : 0;
+    int sum = area1 - inW * inH + area2;
+    return sum;
+}
