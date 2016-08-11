c4e96e5b68f0f6b43d2986d2f975425c491df444
hawkphantomnet <hawkphantomnet@gmail.com>
2016-08-11 22:59:08 +0800

    add cpp solution for Ransom Note

---
+++ RansomNote/Solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> nums(26, 0);
+        for (int i = 0; i < magazine.size(); i++) {
+            nums[magazine[i] - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.size(); i++) {
+            nums[ransomNote[i] - 'a']--;
+            if (nums[ransomNote[i] - 'a'] < 0) return false;
+        }
+        return true;
+    }
+};
