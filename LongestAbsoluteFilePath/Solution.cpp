89bb075391da0988e3f15a1dc95d910ae977c686
hawkphantomnet <hawkphantomnet@gmail.com>
2016-09-09 22:01:21 +0800

    add cpp solution for Longest Absolute File Path

---
+++ LongestAbsoluteFilePath/Solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int lengthLongestPath(string input) {
+        int size = input.size();
+        vector<int> layer(size, 0);
+        int curLen = 0;
+        int curLayer = 0;
+        int maxLen = 0;
+        bool isFile = false;
+        for (int i = 0; i <= size; i++) {
+            if (i == size || input[i] == '\n') {
+                layer[curLayer] = curLen;
+                if (isFile == true) {
+                    int pathLen = 0;
+                    for (int j = 0; j <= curLayer; j++) {
+                        pathLen += layer[j];
+                    }
+                    pathLen += curLayer;
+                    maxLen = maxLen > pathLen ? maxLen : pathLen;
+                }
+                isFile = false;
+                curLayer = 0;
+                curLen = 0;
+            } else if (input[i] == '\t') {
+                curLayer++;
+            } else {
+                curLen++;
+                if (input[i] == '.') {
+                    if (i + 1 < size && input[i + 1] != '\t' && input[i + 1] != '\n') isFile = true;
+                }
+            }
+        }
+        return maxLen;
+    }
+};
