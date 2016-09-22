d20f7ca9db48ccb3788fd1feb22a8a4c0de15d71
hawkphantomnet <hawkphantomnet@gmail.com>
2016-09-22 22:36:07 +0800

    add c solution for Integer Replacement

---
+++ IntegerReplacement/Solution.c
@@ -0,0 +1,13 @@
+int integerReplacement(int n) {
+    int cnt = 0;
+    unsigned int k = n;
+    while (k > 3) {
+        cnt++;
+        if (k & 1) {
+            if (k & 2) k++;
+            else k--;
+        }
+        else k >>= 1;
+    }
+    return cnt + k - 1;
+}
