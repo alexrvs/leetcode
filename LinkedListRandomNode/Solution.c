5e116e39320d11e513b36dc05eb5ab70b7593c70
hawkphantomnet <hawkphantomnet@gmail.com>
2016-10-06 13:08:20 +0800

    add c solution for Linked List Random Node

---
+++ LinkedListRandomNode/Solution.c
@@ -0,0 +1,37 @@
+typedef struct {
+    struct ListNode* head;
+    int len;
+} Solution;
+
+/** @param head The linked list's head. Note that the head is guanranteed to be not null, so it contains at least one node. */
+Solution* solutionCreate(struct ListNode* head) {
+    Solution* s = (Solution*) malloc(sizeof(Solution));
+    s->head = head;
+    s->len = 0;
+    while (head != NULL) {
+        s->len++;
+        head = head->next;
+    }
+    return s;
+}
+
+/** Returns a random node's value. */
+int solutionGetRandom(Solution* obj) {
+    int step = rand() % obj->len;
+    struct ListNode* cur = obj->head;
+    while (step-- != 0) {
+        cur = cur->next;
+    }
+    return cur->val;
+}
+
+void solutionFree(Solution* obj) {
+    free(obj);
+}
+
+/**
+ * Your Solution struct will be instantiated and called as such:
+ * struct Solution* obj = solutionCreate(head);
+ * int param_1 = solutionGetRandom(obj);
+ * solutionFree(obj);
+ */
