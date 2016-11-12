2d2ad3e8febfc7092c6f07e1c37f190bfcef5fef
hawkphantomnet <hawkphantomnet@gmail.com>
2016-11-12 22:55:24 +0800

    add c solution for Minimum Moves To Equal Array Element

---
+++ MinimumMovesToEqualArrayElements/Solution.c
@@ -0,0 +1,13 @@
+int minMoves(int* nums, int numsSize) {
+    int size = numsSize;
+    if (size < 2) return 0;
+    int min = nums[0];
+    for (int i = 1; i < size; i++) {
+        if (min > nums[i]) min = nums[i];
+    }
+    int cnt = 0;
+    for (int i = 0; i < size; i++) {
+        cnt += nums[i] - min;
+    }
+    return cnt;
+}
