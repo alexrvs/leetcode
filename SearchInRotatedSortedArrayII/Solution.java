4496447bf2f950b064c921d046767487c5dd9567
hawkphantomnet <hawkphantomnet@gmail.com>
2016-11-14 22:07:04 +0800

    add java solution for Search In Rotated Sorted Array II

---
+++ SearchInRotatedSortedArrayII/Solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        int size = nums.length;
+        int l = 0;
+        int r = size - 1;
+        int m = 0;
+        while (l <= r) {
+            m = l + (r - l) / 2;
+            if (nums[m] == target) return true;
+            if (nums[l] < nums[m]) {
+                if (nums[l] <= target && target < nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else if (nums[l] > nums[m]) {
+                if (nums[m] < target && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            } else {
+                l++;
+            }
+        }
+        return false;
+    }
+}
