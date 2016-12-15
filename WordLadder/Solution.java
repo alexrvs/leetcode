c2d853f81d5b2007c8dadbcff3f4504e5848588a
hawkphantomnet <hawkphantomnet@gmail.com>
2016-12-16 00:21:46 +0800

    add java solution for Word Ladder

---
+++ WordLadder/Solution.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        Set<String> fromBegin = new HashSet<String>();
+        int wordLen = beginWord.length();
+        fromBegin.add(beginWord);
+        int len = 0;
+        Set<String> visited = new HashSet<String>();
+        while (!fromBegin.isEmpty()) {
+            Set<String> newWords = new HashSet<String>();
+            for (String word : fromBegin) {
+                char[] chars = word.toCharArray();
+                for (int i = 0; i < wordLen; i++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        char old = chars[i];
+                        chars[i] = c;
+                        String newWord = String.valueOf(chars);
+                        
+                        if (newWord.equals(endWord)) {
+                            return len + 2;
+                        }
+                        
+                        if (!visited.contains(newWord) && wordList.contains(newWord)) {
+                            newWords.add(newWord);
+                            visited.add(newWord);
+                        }
+                        
+                        chars[i] = old;
+                    }
+                }
+            }
+            
+            fromBegin = newWords;
+            len++;
+        }
+        
+        return 0;
+    }
+}
