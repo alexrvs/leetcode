a6abb48bad62680d44ebc404fe5347fae92672c1
hawkphantomnet <hawkphantomnet@gmail.com>
2016-12-26 22:24:34 +0800

    add cpp solution for Find Minimum In Rotated Sorted Array II

---
+++ FindMinimumInRotatedSortedArrayII/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int size = nums.size();
+        if (size == 0) return 0;
+        int l = 0;
+        int r = size - 1;
+        int m = 0;
+        while (l < r) {
+            m = l + (r - l) / 2;
+            if (nums[r] < nums[m]) {
+                l = m + 1;
+            } else if (nums[m] < nums[r]) {
+                r = m;
+            } else {
+                if (nums[l] == nums[m]) {
+                    l++;
+                    r--;
+                } else {
+                    r = m;
+                }
+            }
+        }
+        return nums[r];
+    }
+};
