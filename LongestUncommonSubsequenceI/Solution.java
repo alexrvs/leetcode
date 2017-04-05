9ad13408579f03e3a2c32282ae12e9838b3a8512
hawkphantomnet <hawkphantomnet@gmail.com>
2017-04-05 22:15:04 +0800

    add java solution for Longest Uncommon Subsequence I

---
+++ LongestUncommonSubsequenceI/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int findLUSlength(String a, String b) {
+        if (!a.equals(b)) {
+            return Math.max(a.length(), b.length());
+        }
+        return -1;
+    }
+}
