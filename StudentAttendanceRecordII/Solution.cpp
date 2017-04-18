968a96e247d8ed31473366da2456512979b79fec
hawkphantomnet <hawkphantomnet@gmail.com>
2017-04-18 23:01:33 +0800

    add cpp solution for Student Attendance Record II

---
+++ StudentAttendanceRecordII/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int checkRecord(int n) {
+        int mod = 1000000007;
+        long al[2][3] = { {1, 0, 0}, {0, 0, 0} };
+        for (int i = 0; i <= n; i++) {
+            long a = (al[0][0] + al[0][1] + al[0][2]) % mod;
+            al[0][2] = al[0][1];
+            al[0][1] = al[0][0];
+            al[0][0] = a;
+            a += al[1][0] + al[1][1] + al[1][2];
+            a %= mod;
+            al[1][2] = al[1][1];
+            al[1][1] = al[1][0];
+            al[1][0] = a;
+        }
+        return (int) al[1][0];
+    }
+};;w
+k
